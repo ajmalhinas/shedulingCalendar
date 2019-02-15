ba7bee7fb1a335f0a63ce67c701d8f96f1cb9b1f
irfan mohammed <mamirfan92@gmail.com>
2019-02-15 22:21:06 +0530

    all pages connected

--- web/js/webStorage.js
+++ web/js/webStorage.js
@@ -5,7 +5,7 @@ var getSrchItems = (function () {
     return function () {
         var srchItems = {fcr: '', spc: '', fcy: '', loc: '', fm_date: '', to_date: ''};
         if (typeof (Storage) !== "undefined") {
-            if (sessionStorage.srchItems !== undefined && sessionStorage.srchItems !== "{}") {
+            if (sessionStorage.srchItems !== undefined && sessionStorage.srchItems !== "undefined" && sessionStorage.srchItems !== "{}") {
                 srchItems = JSON.parse(sessionStorage.srchItems);
 
             }
@@ -48,12 +48,10 @@ var setEvents = (function () {
     }
 })();
 var getSelEvents = (function () {
-
-
     return function () {
         var events = {};
         if (typeof (Storage) !== "undefined") {
-            if (sessionStorage.SelEvents !== undefined && sessionStorage.SelEvents !== "{}") {
+            if (sessionStorage.SelEvents !== undefined && sessionStorage.SelEvents !== "undefined" && sessionStorage.SelEvents !== "{}") {
                 events = JSON.parse(sessionStorage.SelEvents);
 
             }
@@ -62,5 +60,85 @@ var getSelEvents = (function () {
             document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
         }
         return events;
+    }
+})();
+
+var setUserDetails = (function () {
+
+    return function (userdetails) {
+
+        if (typeof (Storage) !== "undefined") {
+
+            sessionStorage.UserDetails = JSON.stringify(userdetails);
+
+        } else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        }
+        return userdetails;
+    }
+})();
+
+var getUserDetails = (function () {
+    return function () {
+        var userdetails = {};
+        if (typeof (Storage) !== "undefined") {
+            if (sessionStorage.UserDetails !== undefined && sessionStorage.UserDetails !== "undefined" && sessionStorage.UserDetails !== "{}") {
+                userdetails = JSON.parse(sessionStorage.UserDetails);
+
+            }
+            sessionStorage.UserDetails = JSON.stringify(userdetails);
+        } else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        }
+        return userdetails;
+    }
+})();
+
+
+var setPayDetails = (function () {
+
+    return function (paydetails) {
+
+        if (typeof (Storage) !== "undefined") {
+
+            sessionStorage.PayDetails = JSON.stringify(paydetails);
+
+        } else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        }
+        return paydetails;
+    }
+})();
+
+var getPayDetails = (function () {
+    return function () {
+        var paydetails = {};
+        if (typeof (Storage) !== "undefined") {
+            if (sessionStorage.PayDetails !== undefined && sessionStorage.PayDetails !== "undefined" && sessionStorage.PayDetails !== "{}") {
+                paydetails = JSON.parse(sessionStorage.PayDetails);
+
+            }
+            sessionStorage.PayDetails = JSON.stringify(paydetails);
+        } else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        }
+        return paydetails;
+    }
+})();
+
+var destroySession = (function () {
+
+    return function () {
+
+        if (typeof (Storage) !== "undefined") {
+            sessionStorage.srchItems = undefined;
+            sessionStorage.SelEvents = undefined;
+            sessionStorage.UserDetails = undefined;
+            sessionStorage.PayDetails = undefined;
+
+        } else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        }
+
     }
 })();

50424df0fb8a64fbfbbb86c356dd16e8844c1365
irfan mohammed <mamirfan92@gmail.com>
2019-03-23 09:02:44 +0530

    step 1 changes

--- web/js/webStorage.js
+++ web/js/webStorage.js
@@ -19,9 +19,9 @@ var getSrchItems = (function () {
 
 var setSrchItems = (function () {
 
-    return function (fcr_menu, spc_menu, fcy_menu, loc_menu, fm_date, to_date) {
+    return function ( spc_menu, fcy_menu, loc_menu, fm_date, to_date) {
 
-        var srchItems = {fcr: fcr_menu, spc: spc_menu, fcy: fcy_menu, loc: loc_menu, fm_date: fm_date, to_date: to_date};
+        var srchItems = { spc: spc_menu, fcy: fcy_menu, loc: loc_menu, fm_date: fm_date, to_date: to_date};
         if (typeof (Storage) !== "undefined") {
 
             sessionStorage.srchItems = JSON.stringify(srchItems);

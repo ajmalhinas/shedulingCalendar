b439ceda8e00f3b5c1cccb298fb808fc7637f613
irfan mohammed <mamirfan92@gmail.com>
2019-03-28 21:35:01 +0530

    service connected to server

--- web/js/service/SchedulerService.js
+++ web/js/service/SchedulerService.js
@@ -5,7 +5,7 @@ if (dwr.engine == null) dwr.engine = {};
 if (DWREngine == null) var DWREngine = dwr.engine;
 
 if (SchedulerService == null) var SchedulerService = {};
-var isLocal = true;
+var isLocal = false;
 if(isLocal)
     SchedulerService._path = 'http://localhost:8080/qLabV2/dwr';
 else

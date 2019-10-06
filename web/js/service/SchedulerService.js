98c114c602909bf4fa45fcbe5fe11ef28d17a54f
Mohomed Rumaiz <mrumaiz94@gmail.com>
2019-10-06 17:42:05 +0530

    Flow errors corrected

--- web/js/service/SchedulerService.js
+++ web/js/service/SchedulerService.js
@@ -5,7 +5,7 @@ if (dwr.engine == null) dwr.engine = {};
 if (DWREngine == null) var DWREngine = dwr.engine;
 
 if (SchedulerService == null) var SchedulerService = {};
-var isLocal = false;
+var isLocal = true;
 if(isLocal)
     SchedulerService._path = 'http://localhost:8080/qLabV2/dwr';
 else
@@ -21,8 +21,8 @@ SchedulerService.getDate = function(p0, callback) {
 SchedulerService.getServices = function(p0, callback) {
   dwr.engine._execute(SchedulerService._path, 'SchedulerService', 'getServices', p0, callback);
 }
-SchedulerService.getAllFacilitators = function(callback) {
-  dwr.engine._execute(SchedulerService._path, 'SchedulerService', 'getAllFacilitators', callback);
+SchedulerService.getAllFacilitators = function(p0, p1, p2, callback) {
+  dwr.engine._execute(SchedulerService._path, 'SchedulerService', 'getAllFacilitators',p0, p1, p2, callback);
 }
 SchedulerService.getAllSpecialities = function(callback) {
   dwr.engine._execute(SchedulerService._path, 'SchedulerService', 'getAllSpecialities', callback);
